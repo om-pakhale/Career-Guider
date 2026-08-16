4ed566e2d88efef90cce3e8b585d56f22be18cdf
om-pakhale <ompakhale8975@gmail.com>
2026-08-16 23:07:49 +0530

    leaderboard connected to backend/database

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-// supabase-config.js
-const SUPABASE_URL = 'https://ivujvxtzexiqrfvmwcik.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2dWp2eHR6ZXhpcXJmdm13Y2lrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyMzkxNzMsImV4cCI6MjEwMDgxNTE3M30.AfQUADQ34FLa59BQWSEHceGQNuvT7KlOFqSQISOvzEI';
-
-// Global Supabase client instance
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
